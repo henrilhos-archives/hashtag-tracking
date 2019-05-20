7675011d4bf27bb97bf02dc2853d21f0312521f6
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-20 20:33:03 -0300

    :lipstick: Componente para a Toolbar

---
+++ components/Toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <VToolbar color="primary" dark>
+    <VToolbarTitle @click="$router.push('/')">Hashtag Tracking</VToolbarTitle>
+
+    <VSpacer />
+
+    <VToolbarItems class="hidden-sm-and-down">
+      <VBtn flat @click="$router.push('/about')">Sobre</VBtn>
+    </VToolbarItems>
+  </VToolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar'
+}
+</script>
