fd435ff03ebe6bfa77c6a787a6de9b36329962d2
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-20 20:08:52 -0300

    :sparkles: Criando a API

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const consola = require('consola')
+const morgan = require('morgan')
 const { Nuxt, Builder } = require('nuxt')
+
+const config = require('../nuxt.config.js')
+const routes = require('./routes')
+
 const app = express()
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+
+  consola.info('Loading models...')
+  require('./models')
+  consola.success('Models loaded!')
+
+  consola.info('Loading routes...')
+  app.use('/api', routes)
+  consola.success('Routes loaded!')
+
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
@@ -31,4 +47,5 @@ async function start() {
     badge: true
   })
 }
+
 start()
