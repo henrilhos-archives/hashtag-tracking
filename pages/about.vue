a29db82b0bcecd072d25cde8887b2e731a62b2eb
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-20 20:33:45 -0300

    :lipstick: Alterações nos arquivos iniciais

---
+++ pages/about.vue
@@ -0,0 +1,42 @@
+<template>
+  <VLayout wrap>
+    <VFlex xs12 text-xs-center display-1
+      >O Hashtag Tracking foi desenvolvido por:</VFlex
+    >
+    <VFlex xs12 text-xs-center>
+      <VAvatar :size="200" color="grey lighten-4">
+        <VImg
+          src="https://avatars2.githubusercontent.com/u/35281738?s=460&v=4"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        />
+      </VAvatar>
+    </VFlex>
+    <VFlex text-xs-center headline>Henrique de Castilhos</VFlex>
+    <VFlex xs12 text-xs-center>
+      <span v-for="social in socials" :key="social.icon">
+        <a :href="social.url" target="_blank">
+          <VBtn fab dark small color="primary">
+            <VIcon dark>{{ social.icon }}</VIcon>
+          </VBtn>
+        </a>
+      </span>
+    </VFlex>
+  </VLayout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      socials: [
+        {
+          icon: 'mdi-linkedin',
+          url: 'https://www.linkedin.com/in/henriquedecastilhos/'
+        },
+        { icon: 'mdi-github-circle', url: 'https://github.com/castilh0s/' }
+      ]
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,61 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline"
-          >Welcome to the Vuetify + Nuxt.js template</v-card-title
-        >
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-              >issue board</a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <VLayout wrap>
+    <VFlex xs12 title>Hashtags</VFlex>
+    <Hashtags />
+    <VFlex xs12 title>Tweets encontrados</VFlex>
+    <TweetDisplay v-for="(tweet, i) in visibleTweets" :key="i" :tweet="tweet" />
+    <VFlex xs12 text-xs-center>
+      <VPagination
+        v-model="page"
+        color="primary"
+        :length="Math.ceil(tweets.length / pageSize)"
+      />
+    </VFlex>
+  </VLayout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import TweetDisplay from '@/components/TweetDisplay.vue'
+import Hashtags from '@/components/Hashtags.vue'
 
 export default {
+  name: 'Home',
   components: {
-    Logo,
-    VuetifyLogo
+    TweetDisplay,
+    Hashtags
+  },
+  data() {
+    return {
+      tweets: [],
+      visibleTweets: [],
+      page: 1,
+      pageSize: 15
+    }
+  },
+  watch: {
+    page() {
+      this.updateVisibleTweets()
+    }
+  },
+  mounted() {
+    this.getTweets()
+    setInterval(() => {
+      this.getTweets()
+    }, 5000)
+  },
+  methods: {
+    getTweets() {
+      this.$axios.get('/tweets').then(res => {
+        this.tweets = res.data
+        this.updateVisibleTweets()
+      })
+    },
+    updateVisibleTweets() {
+      this.visibleTweets = this.tweets.slice(
+        this.page * this.pageSize - this.pageSize,
+        this.page * this.pageSize
+      )
+    }
   }
 }
 </script>
