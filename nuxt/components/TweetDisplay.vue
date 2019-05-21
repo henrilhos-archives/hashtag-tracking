fc59449e3955f8f14122a96023371a658f32b0fe
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-21 02:51:48 -0300

    :construction: Inserindo testId nos componentes

--- nuxt/components/TweetDisplay.vue
+++ nuxt/components/TweetDisplay.vue
@@ -3,30 +3,20 @@
     <VCard>
       <VCardText>
         <VLayout wrap>
-          <!-- <VFlex xs1 text-xs-center>
-            <VAvatar color="grey lighten-4">
-              <VImg
-                :src="tweet.user.profileImageUrl"
-                :lazy-src="tweet.user.profileImageUrl"
-                aspect-ratio="1"
-                class="grey lighten-2"
-              />
-            </VAvatar>
-          </VFlex> -->
-          <!-- <VFlex xs11> -->
           <VFlex xs12>
             <div class="subheading">
-              <span class="font-weight-medium"
+              <span class="font-weight-medium" data-testid="userName"
                 >{{ tweet.user.name }}&nbsp;</span
               >
               <a
                 :href="`https://twitter.com/${tweet.user.screenName}`"
                 style="text-decoration: none"
                 target="_blank"
+                data-testid="userScreenName"
                 >@{{ tweet.user.screenName }}</a
               >
             </div>
-            <div>{{ tweet.text }}</div>
+            <div data-testid="tweet">{{ tweet.text }}</div>
             <div class="grey--text">
               {{ tweet.createdAt | moment('from') }} ({{
                 tweet.createdAt | moment('DD/MM/YYYY - HH:mm:ss')
