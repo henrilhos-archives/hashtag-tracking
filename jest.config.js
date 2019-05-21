95528661103102328d45d356f356d5b6573ed74f
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-21 02:50:27 -0300

    :wrench: Configurações do Jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  moduleDirectories: ['node_modules', 'nuxt'],
+  moduleDirectories: ['node_modules', 'nuxt', 'server'],
   moduleFileExtensions: ['js', 'vue'],
-  testPathIgnorePatterns: ['/node_modules/'],
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
-  }
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(vuetify))/'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/tests/server/__tests__/components/',
+    '<rootDir>/tests/nuxt/__tests__/components/'
+  ],
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue']
 }
