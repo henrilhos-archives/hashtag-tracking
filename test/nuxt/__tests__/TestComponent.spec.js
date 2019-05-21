b65d06de2fa656741b713ab3044914b42832c3a0
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-21 02:54:01 -0300

    :truck: Movendo o teste exemplo de lugar

--- test/nuxt/__tests__/TestComponent.spec.js
+++ test/nuxt/__tests__/TestComponent.spec.js
@@ -1,6 +1,6 @@
 import 'jest-dom/extend-expect'
 import { render } from 'vue-testing-library'
-import TestComponent from './../nuxt/components/TestComponent'
+import TestComponent from './../../../nuxt/components/TestComponent'
 
 test('should render HelloWorld', () => {
   const { queryByTestId } = render(TestComponent)
