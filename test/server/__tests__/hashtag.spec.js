1731b39f875ecea0e19f8a3573852a05830dfcda
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-23 20:03:52 -0300

    :white_check_mark: WIP - Correção nos testes da hashtag

--- test/server/__tests__/hashtag.spec.js
+++ test/server/__tests__/hashtag.spec.js
@@ -1,13 +1,14 @@
 const request = require('supertest')
-
 const app = require('./../../../server/app')
-describe('create a hashtag', () => {
-  // beforeAll(() => {
-  // app = require('./../../../server/app')
-  // })
 
+let server
+beforeAll(async () => {
+  server = await app.initialize()
+})
+
+describe('create a hashtag', () => {
   test('should make a new hashtag', done => {
-    request(app)
+    request(server)
       .get('/api/hashtag')
       .then(response => {
         // eslint-disable-next-line no-console
