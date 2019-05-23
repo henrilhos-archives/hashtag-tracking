22c6a3319927e7decd02d8657d08da0b60c14377
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-05-23 03:57:30 -0300

    :art: Mudanças na conexão com o banco de dados

--- server/mongo.js
+++ server/mongo.js
@@ -1,30 +1,46 @@
 const mongoose = require('mongoose')
 const consola = require('consola')
+const { Schema } = mongoose
+
 const config = require('./config')
+const hashtag = require('./models/hashtag')
 
-module.exports.connect = () =>
-  new Promise((resolve, reject) => {
-    mongoose.connection.on('connected', () => {
-      if (process.env.NODE_ENV !== 'test') {
-        consola.success('MongoDB connected!')
-      }
-      resolve()
+const srcModels = [hashtag]
+
+const database = mongoose.createConnection(config.MONGODB_URI, {
+  autoReconnect: true,
+  reconnectTries: 0,
+  reconnectInterval: 100,
+  useNewUrlParser: true
+})
+
+const models = {}
+srcModels.forEach(srcModel => {
+  // const srcModel = m
+  srcModel.options.timestamps = true
+  const schema = new Schema(srcModel.definition, srcModel.options)
+
+  if (srcModel.indexes != null) {
+    srcModel.indexes.forEach(idx => {
+      schema.index(idx.fields, idx.options)
     })
+  }
 
-    try {
-      if (process.env.NODE_ENV !== 'test') {
-        consola.info(`Connecting on database...`)
-      }
+  const model = database.model(srcModel.name, schema)
 
-      mongoose
-        .connect(config.MONGODB_URI, {
-          autoReconnect: true,
-          reconnectTries: 0,
-          reconnectInterval: 100,
-          useNewUrlParser: true
-        })
-        .then(resolve, reject)
-    } catch (e) {
-      reject(e)
-    }
-  })
+  consola.info(`Mapping entitiy ${srcModel.name}...`)
+  models[srcModel.name] = model
+})
+
+const getConnection = () => {
+  return database
+}
+
+const getModel = ({ modelName }) => {
+  return models[modelName]
+}
+
+module.exports = {
+  getConnection,
+  getModel
+}
